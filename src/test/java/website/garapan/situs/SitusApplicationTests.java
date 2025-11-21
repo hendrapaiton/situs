ccc9b96fc70ed9b6102e864f804a1dfd53dad19e
Hendra <hendra@example.com>
2025-11-21 22:15:17 +0700

    feat: scaffold initial Spring Boot application with Maven Wrapper

---
+++ src/test/java/website/garapan/situs/SitusApplicationTests.java
@@ -0,0 +1,13 @@
+package website.garapan.situs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
